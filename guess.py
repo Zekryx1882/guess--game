cc292d6fda65f4513a5fab3a14a31a7b87030d72
Zeph <estabilloraf1804@gmail.com>
2025-04-30 23:28:43 +0800

    Add any character

---
+++ guess.py
@@ -0,0 +1,16 @@
+# guess.py
+import random
+
+def main():
+    number = random.randint(1, 10)
+    guess = None
+    while guess != number:
+        guess = int(input("Guess a number 1–10: "))
+        if guess < number:
+            print("Too low!")
+        elif guess > number:
+            print("Too high!")
+    print("You got it!")
+
+if __name__ == "__main__":
+    main()
